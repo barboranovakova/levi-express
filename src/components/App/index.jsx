baca95f483021e5a78ecffba1c5482f7d54a3913
Barbora Nováková <barun.novak@gmail.com>
2024-05-18 10:37:12 +0200

    vypracovani dle prvniho zadani

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
+import { HomePage } from '../../pages/HomePage';
 
 export const App = () => (
   <>
---
+++ src/components/ReservationPage/index.jsx
@@ -0,0 +1,5 @@
+// Dále vytvořte komponentu ReservationPage. Tuto komponentu zobrazte na adrese /reservation. Zatím může také vracet pouze nadpis h2 s textem „Vaše e-jízdenka“, abychom viděli, že se na stránce něco děje. Obsah komponenty vytvoříme později.
+
+export const ReservationPage = () => {
+  return <h2>Vaše e-jízdenka</h2>;
+};
